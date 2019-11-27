af198d49edf4e45ef89a2c7c9974d7be2c8a8d5a
Nicael <n.alves@coblue.com.br>
2019-11-27 16:14:11 -0300

    add path to route]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full'
+    //redirectTo: 'participantes'
+  },
+  {
+    path: 'eventos',
+    loadChildren: './eventos/eventos.module#EventosModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
